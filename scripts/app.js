8500132c74d51580d941cf7da2d1218f393ec908
ceddevstudent89 <cedstudent89@gmail.com>
2024-04-15 13:51:55 +0200

    Modif script nav page

--- scripts/app.js
+++ scripts/app.js
@@ -6,16 +6,30 @@ let imgBtn = document.querySelector(".responsive-nav-btn img");
 btnNav.addEventListener("click", () => {
   listeNav.classList.toggle("active-nav");
 
-  if (imgBtn.src.includes("menu")) {
-    imgBtn.src = "./ressources/croix.svg";
-    imgBtn.style.width = "30px";
-    imgBtn.style.height = "30px";
+  if (window.location.href.includes("index")) {
+    if (imgBtn.src.includes("menu")) {
+      imgBtn.src = "./ressources/croix.svg";
+      imgBtn.style.width = "30px";
+      imgBtn.style.height = "30px";
+    } else {
+      imgBtn.src = "./ressources/menu.svg";
+      imgBtn.style.width = "40px";
+      imgBtn.style.height = "40px";
+    }
   } else {
-    imgBtn.src = "./ressources/menu.svg";
-    imgBtn.style.width = "40px";
-    imgBtn.style.height = "40px";
+    if (imgBtn.src.includes("menu")) {
+      imgBtn.src = "../ressources/croix.svg";
+      imgBtn.style.width = "30px";
+      imgBtn.style.height = "30px";
+    } else {
+      imgBtn.src = "../ressources/menu.svg";
+      imgBtn.style.width = "40px";
+      imgBtn.style.height = "40px";
+    }
   }
 });
+
+console.log(window.location.href);
 
 // Animation FAQ
 const allCross = document.querySelectorAll(".panneau-visible img");
