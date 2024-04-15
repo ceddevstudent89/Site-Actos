3776e97b02dadf2a7f108626c74763c21f351366
ceddevstudent89 <cedstudent89@gmail.com>
2024-04-15 11:43:28 +0200

    Modif chemin image minus plus

--- scripts/app.js
+++ scripts/app.js
@@ -28,7 +28,7 @@ allCross.forEach((logo) => {
     // console.log(height);
 
     if (this.src.includes("plus")) {
-      this.src = "/ressources/minus.svg";
+      this.src = "../ressources/minus.svg";
       currentChoice.style.height = height + 40 + "px";
       currentChoice.style.opacity = 1;
       currentChoice.style.padding = "20px 15px";
