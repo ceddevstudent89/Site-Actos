465d7602db2123b90c8147e87d4f26065951520f
ceddevstudent89 <cedstudent89@gmail.com>
2024-04-15 11:45:01 +0200

    mofif script

--- scripts/app.js
+++ scripts/app.js
@@ -33,7 +33,7 @@ allCross.forEach((logo) => {
       currentChoice.style.opacity = 1;
       currentChoice.style.padding = "20px 15px";
     } else {
-      this.src = "/ressources/plus.svg";
+      this.src = "ressources/plus.svg";
       currentChoice.style.height = 0;
       currentChoice.style.opacity = 0;
       currentChoice.style.padding = "0px 15px";
